29a81ce8c988282cd32735cd912659a2be2cff30
Julio Candanedo <jcandane@asu.edu>
2023-07-26 17:27:25 -0700

    0.1 -> 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as readme:
 setup(
   name = 'bisum',         # How you named your package folder (MyLib)
   packages = ['bisum'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "binary sparse and dense tensor partial-tracing",   # Give a short description about your library
   long_description=long_description,
