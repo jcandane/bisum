01964f2f0dc490825ede4dd3a402fe3a06cf9cb7
Oliver Beckstein <orbeckst@gmail.com>
2023-08-03 10:48:06 -0700

    converted new einsum tests to pytest
    - made print tests proper pytests (removed old code that is tested now)
    - removed test_bisum_outdated.py (new tests supercede)

--- bisum/tests/test_bisum.py
+++ bisum/tests/test_bisum.py
@@ -5,12 +5,16 @@ This python-file contains various tests for the bisum package.
 """
 
 import torch
-#import pytest
+
+import pytest
 
 from bisum import bisum
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
+### The commented out code had not been testing anything. It is left here for historical reasons.
+### It should be turned into proper pytests.
 
 ####c = sptensordot(A.to_sparse(), B.to_sparse(), dims=adjj)
 #s = sdtensordot(A.to_sparse(), B, dims=adjj)
@@ -20,9 +24,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ####print( torch.allclose(C,c.to_dense()) ) #and torch.allclose(C,s.to_dense()) and torch.allclose(C,S.to_dense()) )
 
 ### OUTERPRODUCT
-A = torch.rand(10,10,10, device=device)
-B = torch.rand(10,10,10, device=device)
-adjj = torch.tensor([[],[]], device=device)
+# A = torch.rand(10,10,10, device=device)
+# B = torch.rand(10,10,10, device=device)
+# adjj = torch.tensor([[],[]], device=device)
 
 ####c = sptensordot(A.to_sparse(), B.to_sparse(), dims=adjj)
 ####s = sdtensordot(A.to_sparse(), B, dims=adjj)
@@ -46,56 +50,85 @@ adjj = torch.tensor([[],[]], device=device)
 #print(C.shape, c.shape)
 #print( torch.allclose(C, c.to_dense()) )
 
-"""
-EINSUM TESTS
-"""
+### EINSUM TESTS
 
-einsumstr = "aeacec,cdd -> aed" ## SPARSE-SPARSE, NO post-inter-externals, NO post-transpose
-A = torch.rand(10,10,10,10,10,10, device=device)
-B = torch.rand(10,10,10, device=device)
-print( torch.allclose( bisum( einsumstr, A, B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
+class TestBisum:
+    @pytest.fixture(scope="class")
+    @staticmethod
+    def A():
+        return torch.rand(10,10,10, device=device)
 
-einsumstr = "daa,aed -> a" ## SPARSE-SPARSE, YES post-inter-externals, NO post-transpose
-A = torch.rand(10,10,10, device=device)
-B = torch.rand(10,10,10, device=device)
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
+    @pytest.fixture(scope="class")
+    @staticmethod
+    def B():
+        return torch.rand(10,10,10, device=device)
 
-einsumstr = "daa,aed -> ea" ## SPARSE-SPARSE, YES post-inter-externals, YES post-transpose
-A = torch.rand(10,10,10, device=device)
-B = torch.rand(10,10,10, device=device)
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B).to_dense(), torch.einsum( einsumstr , A, B )) )
+    @pytest.fixture(scope="class")
+    @staticmethod
+    def A2():
+        return torch.rand(10,10,10,10,10,10, device=device)
 
-einsumstr = "daa,aed -> ea" ## SPARSE-DENSE, YES post-inter-externals, YES post-transpose
-A = torch.rand(10,10,10, device=device)
-B = torch.rand(10,10,10, device=device)
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
+    @pytest.fixture(scope="class")
+    @staticmethod
+    def A3():
+        return torch.rand(13,13,13, device=device)
 
-##### if slicing returns an empty array avoid lexsort!!
-einsumstr = "aaa,wop -> a" ## SPARSE-SPARSE, YES post-inter-externals, YES post-transpose
-A = torch.rand(13,13,13, device=device)
-B = torch.rand(10,10,10, device=device)
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
+    @staticmethod
+    def _sparse_sparse(einsumstr, A, B):
+        return bisum(einsumstr, A.to_sparse(), B.to_sparse()).to_dense()
 
-##### if slicing returns an empty array avoid lexsort!
-einsumstr = "qaq,wow -> " ## SPARSE-DENSE, YES post-inter-externals, YES post-transpose
-A = torch.rand(10,10,10, device=device)
-B = torch.rand(10,10,10, device=device)
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A.to_sparse(), B).to_dense(), torch.einsum( einsumstr , A, B )) )
-print( torch.allclose( bisum( einsumstr, A, B.to_sparse()).to_dense(), torch.einsum( einsumstr , A, B )) )
+    @staticmethod
+    def _dense_dense(einsumstr, A, B):
+        return bisum(einsumstr, A, B).to_dense()
+
+    @staticmethod
+    def _sparse_dense(einsumstr, A, B):
+        return bisum(einsumstr, A.to_sparse(), B).to_dense()
+
+    @staticmethod
+    def _dense_sparse(einsumstr, A, B):
+        return bisum(einsumstr, A, B.to_sparse()).to_dense()
+
+    @pytest.fixture(params=[
+        pytest.param(('sparse', 'sparse'), id="sparse/sparse"),
+        pytest.param(('sparse', 'dense'), id="sparse/dense"),
+        pytest.param(('dense', 'sparse'), id="dense/sparse"),
+        pytest.param(('sparse', 'sparse'), id="dense/dense"),
+    ])
+    def bisum_func(self, request):
+        """Return the bisum() invocation with the requested dense/sparse input argument handling."""
+        # This look up table is not super-elegant but fairly transparent (and avoids if/elif)
+        _f = {('sparse', 'sparse'): self._sparse_sparse,
+              ('sparse', 'dense'): self._sparse_dense,
+              ('dense', 'sparse'): self._dense_sparse,
+              ('dense', 'dense'): self._dense_dense,
+              }
+        return _f[request.param]
+
+    def _assert_bisum(self, A, B, einsumstr, bisum_func):
+        C = bisum_func(einsumstr, A, B)
+        ref = torch.einsum(einsumstr , A, B)
+        assert torch.allclose(C, ref)
+
+
+    @pytest.mark.parametrize("einsumstr", [
+        "daa,aed -> a",      # id="YES post-inter-externals, NO post-transpose",
+        "daa,aed -> ea",     # id="YES post-inter-externals, YES post-transpose",
+        "aaa,wop -> a",      # id="empty array/no lexsort, YES post-inter-externals, YES post-transpose",
+        "qaq,wow -> ",       # id="empty array/no lexsort, YES post-inter-externals, YES post-transpose",
+    ])
+    def test_bisum(self, A, B, einsumstr, bisum_func):
+        self._assert_bisum(A, B, einsumstr, bisum_func)
+
+    def test_bisum_NO_post_inter_externals_NO_post_transpose(self, A2, B, bisum_func,
+                                                                           einsumstr="aeacec,cdd -> aed"):
+        # Test needs larger A tensor.
+        #### "NO post-inter-externals, NO post-transpose"
+        self._assert_bisum(A2, B, einsumstr, bisum_func)
+
+
+    def test_bisum_no_lexsort(self, A3, B, bisum_func, einsumstr="aaa,wop -> a"):
+        # Originall, this was a test with a different A tensor.
+        ##### if slicing returns an empty array avoid lexsort!!
+        ## SPARSE-SPARSE, YES post-inter-externals, YES post-transpose
+        self._assert_bisum(A3, B, einsumstr, bisum_func)
